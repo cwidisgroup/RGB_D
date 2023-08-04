a4f891a9c3884771fd2c79d850b063188e47ccaf
ITFCWI-A2LMTC0T\Ashutosh <ashutosh.singla@cwi.nl>
2023-08-04 13:32:15 +0200

    added the commands to get the IR and depth streams.

--- FFmpeg_commands_RGBD.sh
+++ FFmpeg_commands_RGBD.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-
 # to copy the stream from input file to the output file
 ffmpeg -i output_master.mkv -c copy  -map 0:0 temp_0.mkv
+
+# to copy the stream from input file to the output file Depth and IR
+ffmpeg -i output_master.mkv -c:v ffv1 -pix_fmt gray16be -allow_raw_vfw 1 -map 0:1 temp_1.mkv
+ffmpeg -i output_master.mkv -c:v ffv1 -pix_fmt gray16be -allow_raw_vfw 1 -map 0:2 temp_2.mkv
