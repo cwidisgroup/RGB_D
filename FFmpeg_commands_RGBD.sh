e9a5cc2033b17644427c66d59c2c81402f6f0bd1
ITFCWI-A2LMTC0T\Ashutosh <ashutosh.singla@cwi.nl>
2023-08-04 13:38:27 +0200

    to attach json meta data and videos together

--- FFmpeg_commands_RGBD.sh
+++ FFmpeg_commands_RGBD.sh
@@ -12,3 +12,6 @@ ffmpeg -dump_attachment:3 calibration.json -i output_master.mkv
 
 # to show the metadata from all the streams and store them in metadata.txt
 ffprobe -v quiet -show_format -show_streams -print_format json output_master.mkv > metadata_out.txt
+
+# to attach json meta data and video together
+ffmpeg -i temp_0.mkv -i temp_1.mkv -i temp_2.mkv -c copy -map 0:0 -map 1:0 -map 2:0 -allow_raw_vfw 1 -attach calibration.json -metadata:s:3 mimetype=application/octet-stream out.mkv
