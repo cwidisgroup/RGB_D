b06a065794b6028758f9aac65e448afb6032c529
ITFCWI-A2LMTC0T\Ashutosh <ashutosh.singla@cwi.nl>
2023-08-04 13:34:11 +0200

    to get the configuration file.

--- FFmpeg_commands_RGBD.sh
+++ FFmpeg_commands_RGBD.sh
@@ -6,3 +6,6 @@ ffmpeg -i output_master.mkv -c copy  -map 0:0 temp_0.mkv
 # to copy the stream from input file to the output file Depth and IR
 ffmpeg -i output_master.mkv -c:v ffv1 -pix_fmt gray16be -allow_raw_vfw 1 -map 0:1 temp_1.mkv
 ffmpeg -i output_master.mkv -c:v ffv1 -pix_fmt gray16be -allow_raw_vfw 1 -map 0:2 temp_2.mkv
+
+# to get the configuration file 
+ffmpeg -dump_attachment:3 calibration.json -i output_master.mkv
